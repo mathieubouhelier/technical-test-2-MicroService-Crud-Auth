f400c08fab97b8a59c2033d267680aa4024f8c45
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-01-07 07:53:44 -0300

    add migration create-products

---
+++ crud/config/config.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './../.env' });
+
+module.exports ={
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'crud_gptw',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'crud_gptw',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'crud_gptw',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
